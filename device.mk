5d4425a936723550a3c6e7922997a13ddca2ae31
Michael Gernoth <michael@gernoth.net>
2014-11-17 14:00:28 +0100

    keylayout: adapt to 5.0
    Change-Id: I280a62850a545bbdf8e686e63bce53f068578df6
    
    Conflicts:
    	device-proprietary-files.txt

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml
+    $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml \
+    $(DEVICE_FOLDER)/prebuilt/usr/keylayout/cy8c201xx.kl:system/usr/keylayout/cy8c201xx.kl
 
 PRODUCT_PACKAGES += fstab.mapphone_umts
 
